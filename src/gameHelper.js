33f0d5c4ca41fee072ae55cfae18671fc0324fb9
peter_willmott <peter@aerobotics.com>
2021-11-07 23:27:39 +0200

    (fix) Remove gameHelper

--- src/gameHelper.js
+++
@@ -1,7 +0,0 @@
-export const BOARD_WIDTH = 9;
-export const BOARD_HEIGHT = 6;
-
-export const createBoard = () =>
-  Array.from(Array(BOARD_HEIGHT), () =>
-    new Array(BOARD_WIDTH).fill([0, "clear"])
-  );
