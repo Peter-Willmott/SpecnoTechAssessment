6a0a5a8fed775e12ba8c4bf2941d811c5552783c
peter_willmott <peter@aerobotics.com>
2021-11-08 03:45:41 +0200

    (fix) cards src with imports

--- src/components/GameBoard/cardsArray.js
+++ src/components/GameBoard/cardsArray.js
@@ -1,59 +1,115 @@
+import twoClubs from "../../img/cards/2_of_clubs.png";
+import twoDiamonds from "../../img/cards/2_of_diamonds.png";
+import twoHearts from "../../img/cards/2_of_hearts.png";
+import twoSpades from "../../img/cards/2_of_spades.png";
+import threeClubs from "../../img/cards/3_of_clubs.png";
+import threeDiamonds from "../../img/cards/3_of_diamonds.png";
+import threeHearts from "../../img/cards/3_of_hearts.png";
+import threeSpades from "../../img/cards/3_of_spades.png";
+import fourClubs from "../../img/cards/4_of_clubs.png";
+import fourDiamonds from "../../img/cards/4_of_diamonds.png";
+import fourHearts from "../../img/cards/4_of_hearts.png";
+import fourSpades from "../../img/cards/4_of_spades.png";
+import fiveClubs from "../../img/cards/5_of_clubs.png";
+import fiveDiamonds from "../../img/cards/5_of_diamonds.png";
+import fiveHearts from "../../img/cards/5_of_hearts.png";
+import fiveSpades from "../../img/cards/5_of_spades.png";
+import sixClubs from "../../img/cards/6_of_clubs.png";
+import sixDiamonds from "../../img/cards/6_of_diamonds.png";
+import sixHearts from "../../img/cards/6_of_hearts.png";
+import sixSpades from "../../img/cards/6_of_spades.png";
+import sevenClubs from "../../img/cards/7_of_clubs.png";
+import sevenDiamonds from "../../img/cards/7_of_diamonds.png";
+import sevenHearts from "../../img/cards/7_of_hearts.png";
+import sevenSpades from "../../img/cards/7_of_spades.png";
+import eightClubs from "../../img/cards/8_of_clubs.png";
+import eightDiamonds from "../../img/cards/8_of_diamonds.png";
+import eightHearts from "../../img/cards/8_of_hearts.png";
+import eightSpades from "../../img/cards/8_of_spades.png";
+import nineClubs from "../../img/cards/9_of_clubs.png";
+import nineDiamonds from "../../img/cards/9_of_diamonds.png";
+import nineHearts from "../../img/cards/9_of_hearts.png";
+import nineSpades from "../../img/cards/9_of_spades.png";
+import tenClubs from "../../img/cards/10_of_clubs.png";
+import tenDiamonds from "../../img/cards/10_of_diamonds.png";
+import tenHearts from "../../img/cards/10_of_hearts.png";
+import tenSpades from "../../img/cards/10_of_spades.png";
+import jackClubs from "../../img/cards/jack_of_clubs.png" ;
+import jackDiamonds from "../../img/cards/jack_of_diamonds.png" ;
+import jackHearts from "../../img/cards/jack_of_hearts.png" ;
+import jackSpades from "../../img/cards/jack_of_spades.png" ;
+import queenClubs from "../../img/cards/queen_of_clubs.png" ;
+import queenDiamonds from "../../img/cards/queen_of_diamonds.png" ;
+import queenHearts from "../../img/cards/queen_of_hearts.png" ;
+import queenSpades from "../../img/cards/queen_of_spades.png" ;
+import kingClubs from "../../img/cards/king_of_clubs.png" ;
+import kingDiamonds from "../../img/cards/king_of_diamonds.png" ;
+import kingHearts from "../../img/cards/king_of_hearts.png" ;
+import kingSpades from "../../img/cards/king_of_spades.png" ;
+import aceClubs from "../../img/cards/ace_of_clubs.png" ;
+import aceDiamonds from "../../img/cards/ace_of_diamonds.png" ;
+import aceHearts from "../../img/cards/ace_of_hearts.png" ;
+import aceSpades from "../../img/cards/ace_of_spades.png" ;
+import jokerRed from "../../img/cards/joker_red.png" ;
+import jokerBlack from "../../img/cards/joker_black.png" ;
+
+
 // Array of the playing cards
 export const cards = [
-    { src: "/cards/2_of_clubs.png" },
-    { src: "/cards/2_of_diamonds.png" },
-    { src: "/cards/2_of_hearts.png" },
-    { src: "/cards/2_of_spades.png" },
-    { src: "/cards/3_of_clubs.png" },
-    { src: "/cards/3_of_diamonds.png" },
-    { src: "/cards/3_of_hearts.png" },
-    { src: "/cards/3_of_spades.png" },
-    { src: "/cards/4_of_clubs.png" },
-    { src: "/cards/4_of_diamonds.png" },
-    { src: "/cards/4_of_hearts.png" },
-    { src: "/cards/4_of_spades.png" },
-    { src: "/cards/5_of_clubs.png" },
-    { src: "/cards/5_of_diamonds.png" },
-    { src: "/cards/5_of_hearts.png" },
-    { src: "/cards/5_of_spades.png" },
-    { src: "/cards/6_of_clubs.png" },
-    { src: "/cards/6_of_diamonds.png" },
-    { src: "/cards/6_of_hearts.png" },
-    { src: "/cards/6_of_spades.png" },
-    { src: "/cards/7_of_clubs.png" },
-    { src: "/cards/7_of_diamonds.png" },
-    { src: "/cards/7_of_hearts.png" },
-    { src: "/cards/7_of_spades.png" },
-    { src: "/cards/8_of_clubs.png" },
-    { src: "/cards/8_of_diamonds.png" },
-    { src: "/cards/8_of_hearts.png" },
-    { src: "/cards/8_of_spades.png" },
-    { src: "/cards/9_of_clubs.png" },
-    { src: "/cards/9_of_diamonds.png" },
-    { src: "/cards/9_of_hearts.png" },
-    { src: "/cards/9_of_spades.png" },
-    { src: "/cards/10_of_clubs.png" },
-    { src: "/cards/10_of_diamonds.png" },
-    { src: "/cards/10_of_hearts.png" },
-    { src: "/cards/10_of_spades.png" },
-    { src: "/cards/jack_of_clubs.png" },
-    { src: "/cards/jack_of_diamonds.png" },
-    { src: "/cards/jack_of_hearts.png" },
-    { src: "/cards/jack_of_spades.png" },
-    { src: "/cards/queen_of_clubs.png" },
-    { src: "/cards/queen_of_diamonds.png" },
-    { src: "/cards/queen_of_hearts.png" },
-    { src: "/cards/queen_of_spades.png" },
-    { src: "/cards/king_of_clubs.png" },
-    { src: "/cards/king_of_diamonds.png" },
-    { src: "/cards/king_of_hearts.png" },
-    { src: "/cards/king_of_spades.png" },
-    { src: "/cards/ace_of_clubs.png" },
-    { src: "/cards/ace_of_diamonds.png" },
-    { src: "/cards/ace_of_hearts.png" },
-    { src: "/cards/ace_of_spades.png" },
-    { src: "/cards/joker_red.png" },
-    { src: "/cards/joker_black.png" },
+    { src: twoClubs },
+    { src: twoDiamonds },
+    { src: twoHearts },
+    { src: twoSpades },
+    { src: threeClubs },
+    { src: threeDiamonds },
+    { src: threeHearts },
+    { src: threeSpades },
+    { src: fourClubs },
+    { src: fourDiamonds },
+    { src: fourHearts },
+    { src: fourSpades },
+    { src: fiveClubs },
+    { src: fiveDiamonds },
+    { src: fiveHearts },
+    { src: fiveSpades },
+    { src: sixClubs },
+    { src: sixDiamonds },
+    { src: sixHearts },
+    { src: sixSpades },
+    { src: sevenClubs },
+    { src: sevenDiamonds },
+    { src: sevenHearts },
+    { src: sevenSpades },
+    { src: eightClubs },
+    { src: eightDiamonds },
+    { src: eightHearts },
+    { src: eightSpades },
+    { src: nineClubs },
+    { src: nineDiamonds },
+    { src: nineHearts },
+    { src: nineSpades },
+    { src: tenClubs },
+    { src: tenDiamonds },
+    { src: tenHearts },
+    { src: tenSpades },
+    { src: jackClubs },
+    { src: jackDiamonds },
+    { src: jackHearts },
+    { src: jackSpades },
+    { src: queenClubs },
+    { src: queenDiamonds },
+    { src: queenHearts },
+    { src: queenSpades },
+    { src: kingClubs },
+    { src: kingDiamonds },
+    { src: kingHearts },
+    { src: kingSpades },
+    { src: aceClubs },
+    { src: aceDiamonds },
+    { src: aceHearts },
+    { src: aceSpades },
+    { src: jokerRed },
+    { src: jokerBlack },
   ];
 
   export default cards;
